c985f8b97662b5dc6108e71a5bee327b0ee81a82
senth <senth@DESKTOP-B36TCHB>
2020-12-25 20:40:53 +0530

    updated project

--- OrangeHRMAutomationTesting/src/test/java/pages/LoginPage.java
+++ OrangeHRMAutomationTesting/src/test/java/pages/LoginPage.java
@@ -1,10 +1,11 @@
 package pages;
 
+//import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-
+//import org.openqa.selenium.support.ui.Select;
 public class LoginPage {
 	
 	WebDriver driver;
@@ -16,6 +17,20 @@ public class LoginPage {
 	 WebElement password;
 	 @FindBy(xpath="//*[@id='btnLogin']")
 	 WebElement subBtn;
+	 @FindBy(xpath="//*[@id=\"menu_admin_viewAdminModule\"]/b")
+	 WebElement admin3;
+	 @FindBy(xpath="//*[@id=\"searchSystemUser_userName\"]")
+	 WebElement usernam;
+	 @FindBy(xpath="//input[@id='searchSystemUser_employeeName_empName']")
+	 WebElement EmployeeName;
+	 @FindBy(xpath="//input[@id='searchBtn']")
+	 WebElement search;
+	 @FindBy(xpath="//input[@id='ohrmList_chkSelectRecord_31']")
+	 WebElement checkbox;
+	 @FindBy(xpath="//input[@id='btnAdd']")
+	 WebElement add;
+	 
+	 
 	
 	 
 	 public LoginPage(WebDriver driver){
@@ -23,11 +38,22 @@ public class LoginPage {
 		 PageFactory.initElements(driver, this);	 
 	 }
 	 
-	 public void login(String username1,String password1) {
-		 
+	 public void 
+	 
+	 login(String username1,String password1,String name1,String EmployeeName1)   
+	 
+	 
+	 {
 		 username.sendKeys(username1);
 		 password.sendKeys(password1);
 		 subBtn.click();
+		 admin3.click();
+		 usernam.sendKeys(name1);
+		 EmployeeName.sendKeys(EmployeeName1);
+		 search.click();
+		 checkbox.click();
+		 add.click();
+		 
 		
 		 
 		 
--- OrangeHRMAutomationTesting/src/test/java/testcase/TC01_Login.java
+++ OrangeHRMAutomationTesting/src/test/java/testcase/TC01_Login.java
@@ -1,7 +1,10 @@
 package testcase;
 
+import org.openqa.selenium.By;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -13,8 +16,8 @@ public class TC01_Login {
 	@BeforeMethod
 	public void LaunchBorswer() {
 		System.setProperty("webdriver.chrome.driver",
-				"C:\\Users\\Fitaomr3\\Downloads\\chromedriver_win32 (1)\\chromedriver.exe");
-		driver = new ChromeDriver();
+				"C:\\Users\\senth\\Downloads\\Compressed\\chromedriver_win32\\chromedriver.exe");
+		driver = new ChromeDriver(); 
 		driver.get("https://opensource-demo.orangehrmlive.com/index.php/auth/login");
 		driver.manage().window().maximize();
 	}
@@ -22,7 +25,11 @@ public class TC01_Login {
 	@Test
 	public void logintest() {
 		LoginPage obj = new LoginPage(driver);
-		obj.login("Admin", "admin123");
+		obj.login("Admin", "admin123", "Alice.Duval", "Alice Duval");
+		Select drop = new Select(driver.findElement(By.id("searchSystemUser_userType")));
+		drop.selectByVisibleText("ESS");
+		Select drop1 = new Select(driver.findElement(By.id("searchSystemUser_status")));
+		drop1.selectByVisibleText("Enabled");
 	}
 
 }
